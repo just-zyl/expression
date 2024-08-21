b61efcd4c9112ac4990f183ed691a9425e4703d1
Zhangyl <zyl@12345>
2024-08-21 17:40:55 +0800

    初步测试

--- src/test/java/test/Main.java
+++ src/test/java/test/Main.java
@@ -3,6 +3,7 @@ package test;
 import com.utils.expression.exceptions.ExpressionException;
 import com.utils.expression.parser.TokenParser;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -14,23 +15,22 @@ import java.util.Map;
 public class Main {
 
     public static void main(String[] args) throws ExpressionException {
-        Map<String, String> set = new HashMap<>();
-        set.put("name", "李四");
-        set.put("age", "18");
-        //set.contains(1);
-//        String expression = "" +
-//                "IF @set?.containsKey(2) THEN " +
-//                "(IF @set?.contains(1) THEN 1d ELSE 'false' END) " +
-//                "ELSE " +
-//                "2 " +
-//                "END";
-//        String expression = "@set?.contains(1) || @set?.contains(2) && @set?.contains(3) || @set?.contains(4)";
-        String expression = "@name? == 张三 && @age? == 19";
-        String expression2 = "@name? == 李四 && @age? == 18";
 
-        Object execResult = new TokenParser(expression).parse();
-        Object execResult2 = new TokenParser(expression2).parse();
-        System.out.println(execResult);
-        System.out.println(execResult2);
+
+        // 获取模板列表
+        List<String> templateList = new ArrayList<>();
+        templateList.add("@name === '张三' && @age === 18");
+        templateList.add("@name === '李四' && @age === 20");
+        templateList.add("@name === '王五' && @age === 25");
+
+        //业务上需要匹配的人员信息
+        Map<String, Object> map = new HashMap<>();
+        map.put("name", "张三");
+        map.put("age", "18");
+        for (String template : templateList) {
+            TokenParser parser = new TokenParser(template);
+            Object expression = parser.parse().exec(map::get);
+            System.out.println(expression);
+        }
     }
 }
